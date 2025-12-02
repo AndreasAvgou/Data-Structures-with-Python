ef2c56cbc3180c5ca6174d4c5443828bd797183c
Andreas Avgoustis <andreasav@alfaedu.gr>
2025-12-02 10:42:30 +0200

    Delete avl-trees directory

--- avl-trees/node.py
+++
@@ -1,8 +0,0 @@
-# Filename: node.py
-
-class TreeNode(object):
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-        self.height = 1
--- avl-trees/traversal.py
+++
@@ -1,9 +0,0 @@
-# Filename: traversal.py
-
-def preOrder(root):
-    if not root:
-        return
-
-    print("{0} ".format(root.val), end="")
-    preOrder(root.left)
-    preOrder(root.right)
